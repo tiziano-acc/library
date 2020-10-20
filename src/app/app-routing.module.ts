05c3c511969c5af927daf894467a249831ead9d1
Tiziano Pastore <pastoretiziano@gmail.com>
2020-10-20 17:28:49 +0200

    creazione componenti

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { BookInfoComponent } from './book-info/book-info.component';
+import { BooksListComponent } from './books-list/books-list.component';
+
+const routes: Routes = [
+  { path: "book-info", component: BookInfoComponent },
+  { path: "books-list", component: BooksListComponent }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooksListComponent } from './books-list/books-list.component';
+import { BookInfoComponent } from './book-info/book-info.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksListComponent,
+    BookInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
