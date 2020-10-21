3293dd3268778c42679947a7d6681183911491a4
Tiziano Pastore <pastoretiziano@gmail.com>
2020-10-21 13:08:32 +0200

    sviluppo paginazione della tabella

--- src/app/books-list/books-list.component.ts
+++ src/app/books-list/books-list.component.ts
@@ -11,6 +11,11 @@ import { BooksService } from '../books.service';
 export class BooksListComponent implements OnInit {
   public booksList: any;
   public booksQuery = new FormControl();
+  public currentPageBooks = [];
+  public currentPage = 1;
+  public itemsPerPage = 5;
+  public itemsPerPageOptions = [5, 10, 15, 20];
+  public numberOfPagesOptions = [];
 
   constructor(private booksService: BooksService) { }
 
@@ -18,8 +23,41 @@ export class BooksListComponent implements OnInit {
     this.booksQuery.valueChanges.subscribe(query => {
       this.booksService.getBooksByQuery(query).subscribe(response => {
         this.booksList = response;
+        this.updateCurrentPage();
       })
     })
+  }
+
+  createNumberOfPagesOptions() {
+    let numberOfPages = Math.ceil(this.booksList.items.length/this.itemsPerPage);
+
+    for(let i=1; i<=numberOfPages; i++) {
+      this.numberOfPagesOptions.push(i);
+    }
+  }
+
+  extractCurrentPage() {
+    let lastItemIndex = (this.currentPage*this.itemsPerPage) - 1;
+    let firstItemIndex;
+    
+    if(lastItemIndex-this.itemsPerPage < 0) {
+      firstItemIndex = 0;
+    } else {
+      firstItemIndex = lastItemIndex-this.itemsPerPage;
+    }
+
+    for(let i=firstItemIndex; i<=lastItemIndex; i++) {
+      this.currentPageBooks.push(this.booksList.items[i]);
+    }
+
+  }
+
+  updateCurrentPage() {
+    this.currentPageBooks = [];
+    this.numberOfPagesOptions = [];
+    
+    this.createNumberOfPagesOptions();
+    this.extractCurrentPage();
   }
 
 }
--- src/app/books.service.ts
+++ src/app/books.service.ts
@@ -6,11 +6,12 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BooksService {
+  private maxResults = 40;
   private url = 'https://www.googleapis.com/books/v1/volumes';
 
   constructor(private http: HttpClient) { }
 
   getBooksByQuery(query) {
-    return this.http.get(this.url + '?q=' + query + '&maxResults=3');
+    return this.http.get(this.url + '?q=' + query + '&maxResults=' + this.maxResults);
   }
 }
