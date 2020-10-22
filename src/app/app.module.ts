4678cdacd1925da057db482d2700404ef2706fb0
Tiziano Pastore <pastoretiziano@gmail.com>
2020-10-22 10:13:09 +0200

    creazione pagina di dettaglio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt, 'it');
+
 import { AppComponent } from './app.component';
 import { BooksListComponent } from './books-list/books-list.component';
 import { BookInfoComponent } from './book-info/book-info.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+
 import { BooksService } from './books.service';
 import { TruncatePipe } from './truncate.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksListComponent,
     BookInfoComponent,
+    BooksListComponent,
     TruncatePipe
   ],
   imports: [
--- src/app/book-info/book-info.component.ts
+++ src/app/book-info/book-info.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-book-info',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-info.component.css']
 })
 export class BookInfoComponent implements OnInit {
+  public book: any;
+  public coverLink: string;
+  public infoLink: string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.book = window.history.state.volumeInfo;
+    this.coverLink = window.history.state.volumeInfo.imageLinks.smallThumbnail;
+    this.infoLink = window.history.state.volumeInfo.infoLink;
   }
 
 }
